cec4e5d7553fd1acfac913e113009301e3e12d7d
StanfordDC <stanforddchandra@gmail.com>
2022-08-22 22:43:30 +0800

    Added FOK order

---
+++ FOKOrder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.text.DecimalFormat;
+public class FOKOrder extends Order{
+
+    static final StockExchange stockExchange = StockExchange.getStockExchange();
+    static final HashMap<Order, Integer> sellRecords = stockExchange.getSellOrders();
+    static final HashMap<Order, Integer> buyRecords = stockExchange.getBuyOrders();
+    static final ArrayList<Order> orderRecords = stockExchange.getOrderRecords();
+    
+    public FOKOrder(String name, double price, int quantity, Side side, Type type){
+        super(name, price, quantity, side, type);
+    }
+
+    @Override 
+}
