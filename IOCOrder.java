b99bb3772f7c58544e92110f21c654675fa89d03
StanfordDC <stanforddchandra@gmail.com>
2022-08-22 23:12:32 +0800

    IOC order methods complete

--- IOCOrder.java
+++ IOCOrder.java
@@ -1,16 +1,15 @@
 import java.util.*;
 import java.text.DecimalFormat;
-public class IOCOrder extends Order{
-
-    static final StockExchange stockExchange = StockExchange.getStockExchange();
-    static final HashMap<Order, Integer> sellRecords = stockExchange.getSellOrders();
-    static final HashMap<Order, Integer> buyRecords = stockExchange.getBuyOrders();
-    static final ArrayList<Order> orderRecords = stockExchange.getOrderRecords();
+public class IOCOrder extends LimitOrder{
     
     public IOCOrder(String name, double price, int quantity, Side side, Type type){
         super(name, price, quantity, side, type);
     }
 
-    @Override 
+    @Override
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("0.00");
+        return "You have placed an IOC "+this.getSide().toLower()+" order for "+this.getQuantity()+" "+this.getName()+" shares at $"+df.format(this.getPrice())+" each";
+    }
     
 }
